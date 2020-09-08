51992d939eee528cd7f8eb4b1ace471177df2b9a
hemapriya <hemapriyasampath@gmail.com>
2020-09-08 19:17:31 -0400

    change to className attributes for primary and warning count

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -5,9 +5,12 @@ class Counter extends Component {
     count: 5,
   };
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "primary" : "warning";
+
     return (
       <>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </>
     );
