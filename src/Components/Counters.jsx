20062155323281f5e079b5e015f48bd6314ebfcc
hemapriya <hemapriyasampath@gmail.com>
2020-09-11 18:59:05 -0400

    using map() for counter components

--- src/Components/Counters.jsx
+++ src/Components/Counters.jsx
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import Counter from "./Counter";
 
 class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 1 },
+      { id: 3, value: 1 },
+      { id: 4, value: 1 },
+    ],
+  };
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+        {this.state.counters.map((counter) => (
+          <Counter key={counter.id} />
+        ))}
       </div>
     );
   }
