cb8269a0bdbe4d8a15554fab66f9afc017bc5bec
hemapriya <hemapriyasampath@gmail.com>
2020-09-13 08:38:53 -0400

    encapulating counter object

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleOnClick = (value) => {
@@ -23,7 +23,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm"
         >
           Delete
